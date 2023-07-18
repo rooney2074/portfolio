5ab95b1f4254904fcf0d65f16ecc3894c116910b
rooney2074 <rony47official@gmail.com>
2023-07-18 12:33:44 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,25 +1,4 @@
-let counts=setInterval(updated);
-        let upto=0;
-        function updated(){
-            var count= document.getElementById("counter");
-            count.innerHTML=++upto;
-            if(upto===80)
-            {
-                clearInterval(counts);
-            }
-        }
 
-        
-
-const progressbar = document.querySelector(".progress");
-const changeProgress = (progress) => {
-  progressbar.style.width = `${progress}%`;
-};
-
-/* change progress after 1 second (only for showcase) */
-setTimeout(() => changeProgress(22), 100);
-setTimeout(() => changeProgress(45), 200);
-setTimeout(() => changeProgress(80), 300);
 
 
 // preloader
